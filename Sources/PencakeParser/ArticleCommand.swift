f533e7a32aef720ebc00454a3815034ab0d07c89
Harumaru169 <kosei.haruyama@icloud.com>
2022-01-20 22:40:28 +0900

    Move 'ArticleCommand' from the scope of 'PencakeCommand' to the global scope.

--- Sources/PencakeParser/ArticleCommand.swift
+++ Sources/PencakeParser/ArticleCommand.swift
@@ -10,63 +10,61 @@ import Foundation
 import PencakeParserCore
 import ArgumentParser
 
-extension PencakeCommand {
+struct ArticleCommand: AsyncParsableCommand {
     
-    struct ArticleCommand: AsyncParsableCommand {
-        static let configuration: CommandConfiguration = .init(
-            commandName: "article",
-            abstract: "Parses a Pencake article and prints in JSON format."
-        )
-        
-        @Argument(
-            help: "Path to the file to read.",
-            completion: .file(extensions: ["txt"]),
-            transform: { string in
-                guard FileManager.default.fileExists(atPath: string) else {
-                    throw ExecutionError.fileDoesNotExist(path: string)
-                }
-                return string
-            })
-        var path: String
-        
-        @Option(
-            name: [.short, .customLong("lang")],
-            help: "Language of the article. This will be used to parse dates.",
-            completion: .list(Language.allCases.map(\.rawValue)),
-            transform: { string in
-                guard let result = Language(rawValue: string) else {
-                    throw ExecutionError.invalidLanguage
-                }
-                return result
-            })
-        var language: Language = .english
-        
-        @Flag(
-            name: [.customLong("pretty-printed"), .customShort("p")],
-            help: "Print the JSON contents in pretty printed style."
-        )
-        var isFormatPrettyPrinted = false
-        
-        func runAsync() async throws {
-            guard let data = FileManager.default.contents(atPath: path) else {
-                throw ExecutionError.failedToReadData
+    static let configuration: CommandConfiguration = .init(
+        commandName: "article",
+        abstract: "Parses a Pencake article and prints in JSON format."
+    )
+    
+    @Argument(
+        help: "Path to the file to read.",
+        completion: .file(extensions: ["txt"]),
+        transform: { string in
+            guard FileManager.default.fileExists(atPath: string) else {
+                throw ExecutionError.fileDoesNotExist(path: string)
             }
-            
-            let article = try await ArticleParser().parse(from: data, language: language)
-            
-            let jsonEncoder = JSONEncoder()
-            if isFormatPrettyPrinted {
-                jsonEncoder.outputFormatting = .prettyPrinted
+            return string
+        })
+    var path: String
+    
+    @Option(
+        name: [.short, .customLong("lang")],
+        help: "Language of the article. This will be used to parse dates.",
+        completion: .list(Language.allCases.map(\.rawValue)),
+        transform: { string in
+            guard let result = Language(rawValue: string) else {
+                throw ExecutionError.invalidLanguage
             }
-            jsonEncoder.dateEncodingStrategy = .iso8601
-            let jsonData = try jsonEncoder.encode(article)
-            
-            print(String(data: jsonData, encoding: .utf8)!)
+            return result
+        })
+    var language: Language = .english
+    
+    @Flag(
+        name: [.customLong("pretty-printed"), .customShort("p")],
+        help: "Print the JSON contents in pretty printed style."
+    )
+    var isFormatPrettyPrinted = false
+    
+    func runAsync() async throws {
+        guard let data = FileManager.default.contents(atPath: path) else {
+            throw ExecutionError.failedToReadData
         }
+        
+        let article = try await ArticleParser().parse(from: data, language: language)
+        
+        let jsonEncoder = JSONEncoder()
+        if isFormatPrettyPrinted {
+            jsonEncoder.outputFormatting = .prettyPrinted
+        }
+        jsonEncoder.dateEncodingStrategy = .iso8601
+        let jsonData = try jsonEncoder.encode(article)
+        
+        print(String(data: jsonData, encoding: .utf8)!)
     }
 }
 
-extension PencakeCommand.ArticleCommand {
+extension ArticleCommand {
     enum ExecutionError: Error, CustomStringConvertible {
         case fileDoesNotExist(path: String)
         
