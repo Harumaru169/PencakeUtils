a2052c00d11b3e60f10403322c25abec0a72524e
Harumaru169 <kosei.haruyama@icloud.com>
2022-01-31 23:13:46 +0900

    Change 'NewlineReplacer' from struct to class.

--- Sources/PencakeParserCore/NewlineReplacer.swift
+++ Sources/PencakeParserCore/NewlineReplacer.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Regex
 
-public struct NewlineReplacer: NewlineReplacerProtocol {
+public class NewlineReplacer: NewlineReplacerProtocol {
     
     public init() {}
     
