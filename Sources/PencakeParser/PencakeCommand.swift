f889fe1e7d4b8b7e4998a96c2cbb469b57314451
Harumaru169 <kosei.haruyama@icloud.com>
2022-01-31 12:44:20 +0900

    Update version string in 'PencakeCommand.swift' and 'README.md'.

--- Sources/PencakeParser/PencakeCommand.swift
+++ Sources/PencakeParser/PencakeCommand.swift
@@ -14,7 +14,7 @@ struct PencakeCommand: ParsableCommand {
         commandName: "pencakeparser",
         abstract: "Pencake Utilities.",
         discussion: "",
-        version: "version 0.4.0",
+        version: "version 0.4.1",
         shouldDisplay: true,
         subcommands: [ArticleCommand.self, StoryCommand.self],
         defaultSubcommand: StoryCommand.self,
