5f8dff8ae27ce3565d1da0394ac1f9330a7d98bf
Harumaru169 <kosei.haruyama@icloud.com>
2022-02-20 19:34:44 +0900

    Add 'StoryInformation'.

---
+++ Sources/PencakeParserCore/StoryInformation.swift
@@ -0,0 +1,40 @@
+//
+//  StoryInformation.swift
+//  
+//
+//  Created by k.haruyama on 2022/02/20.
+//  
+//
+
+import Foundation
+
+public struct StoryInformation: Equatable {
+    public var title: String
+    
+    public var subtitle: String
+    
+    public var createdDate: Date
+    
+    public var exportedDate: Date
+    
+    public var articleCount: Int
+    
+    public init(title: String, subtitle: String, createdDate: Date, exportedDate: Date, articleCount: Int) {
+        self.title = title
+        self.subtitle = subtitle
+        self.createdDate = createdDate
+        self.exportedDate = exportedDate
+        self.articleCount = articleCount
+    }
+}
+
+extension Story {
+    // 'information.articleCount' will be ignored.
+    public init(information: StoryInformation, articles: [Article]) {
+        self.title = information.title
+        self.subtitle = information.subtitle
+        self.createdDate = information.createdDate
+        self.exportedDate = information.exportedDate
+        self.articles = articles
+    }
+}
