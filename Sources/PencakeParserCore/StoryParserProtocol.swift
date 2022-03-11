2a8fe45a93875c9999134209f9e9c6fe53df4fbf
Harumaru169 <kosei.haruyama@icloud.com>
2022-03-11 16:58:15 +0900

    Remove methods of 'StoryParserProtocol' which take 'Data' instead of 'URL', and deprecate method which takes 'Language' instead of 'ParseOptions'.

--- Sources/PencakeParserCore/StoryParserProtocol.swift
+++ Sources/PencakeParserCore/StoryParserProtocol.swift
@@ -9,22 +9,11 @@
 import Foundation
 
 public protocol StoryParserProtocol: Decodable, Sendable {
-    @available(*, deprecated, message: "Use 'parse(storyInfoData:articleDatas:options:)' instead.")
-    func parse(storyInfoData: Data, articleDatas: [Data], language: Language) async throws -> Story
-    
-    @available(*, deprecated, message: "Use 'parse(directoryURL:options:)' instead.")
-    func parse(directoryURL: URL, language: Language) async throws -> Story
-    
-    func parse(storyInfoData: Data, articleDatas: [Data], options: ParseOptions) async throws -> Story
-    
     func parse(directoryURL: URL, options: ParseOptions) async throws -> Story
 }
 
 extension StoryParserProtocol {
-    public func parse(storyInfoData: Data, articleDatas: [Data], language: Language) async throws -> Story {
-        return try await parse(storyInfoData: storyInfoData, articleDatas: articleDatas, options: .init(language: language))
-    }
-    
+    @available(*, deprecated, message: "Use 'parse(directoryURL:options:)' instead.")
     public func parse(directoryURL: URL, language: Language) async throws -> Story {
         return try await parse(directoryURL: directoryURL, options: .init(language: language))
     }
