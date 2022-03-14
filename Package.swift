0030db8917c66dce6f28e6c5e85fc9751b19887e
Harumaru169 <kosei.haruyama@icloud.com>
2022-03-14 19:57:53 +0900

    Add package dependency on ZIPFoundation.

--- Package.swift
+++ Package.swift
@@ -31,13 +31,17 @@ let package = Package(
             url: "https://github.com/apple/swift-argument-parser",
             from: "1.0.2"
         ),
+        .package(
+            url: "https://github.com/weichsel/ZIPFoundation",
+            from: "0.9.14"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PencakeParserCore",
-            dependencies: ["Regex"]
+            dependencies: ["Regex", "ZIPFoundation"]
         ),
         .executableTarget(
             name: "PencakeParser",
