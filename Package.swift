634aaf811b954e963efa2dfd1551b76c62658f9d
Harumaru169 <kosei.haruyama@icloud.com>
2022-03-15 16:37:04 +0900

    Make major changes to the contents of Package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "PencakeParser",
+    name: "PencakeUtils",
     platforms: [
         .macOS(.v12),
         .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PencakeParserCore",
-            targets: ["PencakeParserCore"]
+            name: "Pencake",
+            targets: ["PencakeParser", "PencakeBuilder"]
+        ),
+        .library(
+            name: "PencakeParser",
+            targets: ["PencakeParser"]
+        ),
+        .library(
+            name: "PencakeBuilder",
+            targets: ["PencakeBuilder"]
         ),
         .executable(
-            name: "pencakeparser",
-            targets: ["PencakeParser"]
+            name: "pencake",
+            targets: ["PencakeCLI"]
         )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/crossroadlabs/Regex",
             from: "1.2.0"
@@ -37,30 +42,40 @@ let package = Package(
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PencakeParserCore",
-            dependencies: ["Regex", "ZIPFoundation"]
+            name: "PencakeCore"
         ),
-        .executableTarget(
+        .target(
             name: "PencakeParser",
             dependencies: [
-                "PencakeParserCore",
-                .product(
-                    name: "ArgumentParser",
-                    package: "swift-argument-parser",
-                    condition: nil
-                )
+                "PencakeCore",
+                .product(name: "Regex", package: "Regex"),
+                .product(name: "ZIPFoundation", package: "ZIPFoundation")
             ]
         ),
+        .target(
+            name: "PencakeBuilder",
+            dependencies: [
+                "PencakeCore",
+                .product(name: "ZIPFoundation", package: "ZIPFoundation")
+            ]
+        ),
+        .executableTarget(
+            name: "PencakeCLI",
+            dependencies: [
+                "PencakeParser",
+                "PencakeBuilder",
+                .product(name:"ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
+        //MARK: - Test Targets
         .testTarget(
             name: "PencakeParserTests",
             dependencies: ["PencakeParser"]
         ),
         .testTarget(
-            name: "PencakeParserCoreTests",
-            dependencies: ["PencakeParserCore"]
+            name: "PencakeBuilderTests",
+            dependencies: ["PencakeBuilder"]
         )
     ]
 )
