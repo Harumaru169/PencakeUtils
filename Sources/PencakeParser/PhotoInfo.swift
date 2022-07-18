c4499c6e372020bac3b73fadc4324638b165b484
Harumaru169 <kosei.haruyama@icloud.com>
2022-07-18 16:28:17 +0900

    Add PhotoInfo in PencakeParser

---
+++ Sources/PencakeParser/PhotoInfo.swift
@@ -0,0 +1,14 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Kosei Haruyama.
+//
+
+import Foundation
+
+internal struct PhotoInfo: Sendable {
+    var url: URL
+    var articleNumber: Int
+    var photoNumber: Int
+    var fileExtension: String
+}
