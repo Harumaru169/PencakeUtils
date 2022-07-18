85a60902d571cbea33cbd70a6164e3f7b89affa0
Harumaru169 <kosei.haruyama@icloud.com>
2022-07-18 18:56:50 +0900

    Replace existing regex with Swift native regex and change implementation according to the changes in protocol definitions

--- Sources/PencakeParser/Parallel/ParallelPhotosLoader.swift
+++ Sources/PencakeParser/Parallel/ParallelPhotosLoader.swift
@@ -6,67 +6,95 @@
 
 import Foundation
 import PencakeCore
-import Regex
+import RegexBuilder
 
 public final class ParallelPhotosLoader: ParallelPhotosLoaderProtocol {
     
     public init() {}
-
-    private static let fileNameRegex = try! Regex(
-        pattern: #"^IMG_([0-9]+)_([0-9]+)\.([^\.]+)$"#,
-        groupNames: "articleNumber", "photoNumber", "fileExtension"
-    )
     
-    public func load(from directoryURL: URL, articleNumber specifiedArticleNumber: Int?) async throws -> [Photo] {
-        let fileManager = FileManager.default
-        
-        let fileType = try fileManager.type(at: directoryURL)
-        guard fileType == .typeDirectory else {
-            throw ParseError.unexpectedFileType(path: directoryURL.path, expected: .typeDirectory, actual: fileType)
+    private let fileManager = FileManager.default
+    
+    private let fileNameRegex = Regex {
+        "IMG_"
+        TryCapture(OneOrMore(.digit)) { substring in
+            Int(substring)
         }
+        "_"
+        TryCapture(OneOrMore(.digit)) { substring in
+            Int(substring)
+        }
+        "."
+        Capture {
+            OneOrMore {
+                CharacterClass.anyOf(".").inverted
+            }
+        }
+    }
+    
+    public func loadAllPhotos(in directoryURL: URL) async throws -> [Photo] {
+        try checkURLIndicatesDirectory(url: directoryURL)
         
         let photoURLs: [URL] = try fileManager.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
         
         return try await withThrowingTaskGroup(of: Photo?.self, returning: [Photo].self) { group in
             for photoURL in photoURLs {
                 _ = group.addTaskUnlessCancelled {
-                    let fileName = photoURL.lastPathComponent
-                    
-                    guard let match = Self.fileNameRegex.findFirst(in: fileName) else {
-                        return nil
-                    }
-                    
-                    let articleNumberString = match.group(named: "articleNumber")!
-                    let articleNumber = Int(articleNumberString)!
-                    
-                    if let specifiedArticleNumber = specifiedArticleNumber, articleNumber != specifiedArticleNumber {
-                        return nil
-                    }
-                            
-                    let photoNumberString = match.group(named: "photoNumber")!
-                    let photoNumber = Int(photoNumberString)!
-                    
-                    guard let photoData = fileManager.contents(atPath: photoURL.path) else {
-                        throw ParseError.failedToReadFile(path: photoURL.path)
-                    }
-                    
-                    return Photo(
-                        data: photoData,
-                        fileExtension: photoURL.pathExtension,
-                        isTrimmedCoverPhoto: photoNumber == 0
-                    )
+                    guard let photoInfo = self.extractPhotoInfo(from: photoURL) else { return nil }
+                    return try self.makePhoto(from: photoInfo)
                 }
             }
             
-            var results: [Photo] = []
-            
-            for try await photo in group {
-                if let photo = photo {
-                    results.append(photo)
-                }
-            }
-            
-            return results
+            return try await Array(group.compactMap { $0 })
         }
+    }
+    
+    public func loadPhotos(ofArticleNumber specifiedArticleNumber: Int, in directoryURL: URL) async throws -> [Photo] {
+        try checkURLIndicatesDirectory(url: directoryURL)
+        
+        let photoURLs = try fileManager.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        
+        return try await withThrowingTaskGroup(of: Photo?.self, returning: [Photo].self) { group in
+            for photoURL in photoURLs {
+                _ = group.addTaskUnlessCancelled { [self] in
+                    guard let photoInfo = extractPhotoInfo(from: photoURL) else { return nil }
+                    guard photoInfo.articleNumber == specifiedArticleNumber else { return nil }
+                    return try makePhoto(from: photoInfo)
+                }
+            }
+            
+            return try await Array(group.compactMap { $0 })
+        }
+    }
+    
+    private func checkURLIndicatesDirectory(url: URL) throws {
+        let fileType = try fileManager.type(at: url)
+        guard fileType == .typeDirectory else {
+            throw ParseError.unexpectedFileType(path: url.path, expected: .typeDirectory, actual: fileType)
+        }
+    }
+    
+    private func extractPhotoInfo(from url: URL) -> PhotoInfo? {
+        let fileName = url.lastPathComponent
+        guard let output = fileName.wholeMatch(of: fileNameRegex)?.output else { return nil }
+        return PhotoInfo(url: url, articleNumber: output.1, photoNumber: output.2, fileExtension: String(output.3))
+    }
+    
+    private func makePhoto(from photoInfo: PhotoInfo) throws -> Photo {
+        assert(photoInfo.url.pathExtension == photoInfo.fileExtension)
+        
+        guard let photoData = fileManager.contents(atPath: photoInfo.url.path) else {
+            throw ParseError.failedToReadFile(path: photoInfo.url.path)
+        }
+        
+        return Photo(data: photoData, fileExtension: photoInfo.fileExtension, isTrimmedCoverPhoto: photoInfo.photoNumber == 0)
+    }
+}
+
+//FIXME: PencakeCoreへ移動させる
+extension Array {
+    init<S>(_ asyncSequence: S) async rethrows where Element == S.Element, S : AsyncSequence {
+        self = try await asyncSequence.reduce(into: [Element](), { partialResult, element in
+            partialResult.append(element)
+        })
     }
 }
--- Sources/PencakeParser/PhotosLoader.swift
+++ Sources/PencakeParser/PhotosLoader.swift
@@ -6,53 +6,96 @@
 
 import Foundation
 import PencakeCore
-import Regex
+import RegexBuilder
 
 public final class PhotosLoader: PhotosLoaderProtocol {
     
     public init() {}
     
-    private static let fileNameRegex = try! Regex(
-        pattern: #"^IMG_([0-9]+)_([0-9]+)\.([^\.]+)$"#,
-        groupNames: "articleNumber", "photoNumber", "fileExtension"
-    )
+    private let fileManager = FileManager.default
     
+    private let fileNameRegex = Regex {
+        "IMG_"
+        TryCapture(OneOrMore(.digit)) { substring in
+            Int(substring)
+        }
+        "_"
+        TryCapture(OneOrMore(.digit)) { substring in
+            Int(substring)
+        }
+        "."
+        Capture {
+            OneOrMore {
+                CharacterClass.anyOf(".").inverted
+            }
+        }
+    }
+    
+    @available(*, deprecated, message: "Use loadAllPhotos(in:) or loadPhotos(ofArticleNumber:in:) instead.")
     public func load(from directoryURL: URL, articleNumber specifiedArticleNumber: Int?) throws -> [Photo] {
-        let fileManager = FileManager.default
         
         let fileType = try fileManager.type(at: directoryURL)
         guard fileType == .typeDirectory else {
             throw ParseError.unexpectedFileType(path: directoryURL.path, expected: .typeDirectory, actual: fileType)
         }
         
+        if let specifiedArticleNumber {
+            return try loadPhotos(ofArticleNumber: specifiedArticleNumber, in: directoryURL)
+        } else {
+            return try loadAllPhotos(in: directoryURL)
+        }
+    }
+    
+    public func loadAllPhotos(in directoryURL: URL) throws -> [Photo] {
+        try checkURLIndicatesDirectory(url: directoryURL)
+        
         let photoURLs: [URL] = try fileManager.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
         
-        return try photoURLs.compactMap { photoURL throws -> Photo? in
-            let fileName = photoURL.lastPathComponent
-            
-            guard let match = Self.fileNameRegex.findFirst(in: fileName) else {
-                return nil
+        return try photoURLs
+            .compactMap { extractPhotoInfo(from: $0) }
+            .map { try makePhoto(from: $0) }
+    }
+    
+    public func loadPhotos(ofArticleNumber specifiedArticleNumber: Int, in directoryURL: URL) throws -> [Photo] {
+        try checkURLIndicatesDirectory(url: directoryURL)
+        
+        let photoURLs: [URL] = try fileManager.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        
+        let lazyPhotos = try photoURLs
+            .lazy
+            .compactMap { (photoURL: URL) -> PhotoInfo? in
+                self.extractPhotoInfo(from: photoURL)
             }
-            
-            let articleNumberString = match.group(named: "articleNumber")!
-            let articleNumber = Int(articleNumberString)!
-            
-            if let specifiedArticleNumber = specifiedArticleNumber, articleNumber != specifiedArticleNumber {
-                return nil
+            .filter { (photoInfo: PhotoInfo) -> Bool in
+                photoInfo.articleNumber == specifiedArticleNumber
             }
-            
-            let photoNumberString = match.group(named: "photoNumber")!
-            let photoNumber = Int(photoNumberString)!
-            
-            guard let photoData = fileManager.contents(atPath: photoURL.path) else {
-                throw ParseError.failedToReadFile(path: photoURL.path)
+            .map { (photoInfo: PhotoInfo) -> Photo in
+                try makePhoto(from: photoInfo)
             }
-            
-            return Photo(
-                data: photoData,
-                fileExtension: photoURL.pathExtension,
-                isTrimmedCoverPhoto: photoNumber == 0
-            )
+        
+        return Array(lazyPhotos)
+    }
+    
+    private func checkURLIndicatesDirectory(url: URL) throws {
+        let fileType = try fileManager.type(at: url)
+        guard fileType == .typeDirectory else {
+            throw ParseError.unexpectedFileType(path: url.path, expected: .typeDirectory, actual: fileType)
         }
+    }
+    
+    private func extractPhotoInfo(from url: URL) -> PhotoInfo? {
+        let fileName = url.lastPathComponent
+        guard let output = fileName.wholeMatch(of: fileNameRegex)?.output else { return nil }
+        return PhotoInfo(url: url, articleNumber: output.1, photoNumber: output.2, fileExtension: String(output.3))
+    }
+    
+    private func makePhoto(from photoInfo: PhotoInfo) throws -> Photo {
+        assert(photoInfo.url.pathExtension == photoInfo.fileExtension)
+        
+        guard let photoData = fileManager.contents(atPath: photoInfo.url.path) else {
+            throw ParseError.failedToReadFile(path: photoInfo.url.path)
+        }
+        
+        return Photo(data: photoData, fileExtension: photoInfo.fileExtension, isTrimmedCoverPhoto: photoInfo.photoNumber == 0)
     }
 }
